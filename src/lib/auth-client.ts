9bfe2b1749adee3135127223e77c16b9b6e131e3
Arnold Agaba <arnoldagaba000@gmail.com>
2026-02-17 20:45:26 +0300

    feat: Add last login plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,10 @@
+import {
+    inferAdditionalFields,
+    lastLoginMethodClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import type { auth } from "./auth";
 
-export const authClient = createAuthClient({});
+export const authClient = createAuthClient({
+    plugins: [inferAdditionalFields<typeof auth>(), lastLoginMethodClient()],
+});
