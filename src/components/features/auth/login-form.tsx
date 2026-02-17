ad27bf47eb2047553d8304ab5949da104d27e522
Arnold Agaba <arnoldagaba000@gmail.com>
2026-02-17 20:48:44 +0300

    chore: Create auth forms and google logo

---
+++ src/components/features/auth/login-form.tsx
@@ -0,0 +1,246 @@
+import { revalidateLogic, useForm } from "@tanstack/react-form";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { useTransition } from "react";
+import toast from "react-hot-toast";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { loginSchema } from "@/schemas/auth-schema";
+import GoogleLogo from "./google-logo";
+
+const LoginForm = () => {
+    const navigate = useNavigate();
+    const [isPending, startTransition] = useTransition();
+
+    const form = useForm({
+        defaultValues: { email: "", password: "", rememberMe: false },
+        onSubmit: ({ value }) => {
+            startTransition(async () => {
+                await authClient.signIn.email(
+                    { ...value },
+                    {
+                        onSuccess: (ctx) => {
+                            navigate({ to: "/", replace: true });
+                            toast.success(
+                                `Welcome back, ${ctx.data.user.name}`
+                            );
+                        },
+                        onError: (ctx) => {
+                            toast.error(ctx.error.message);
+                        },
+                    }
+                );
+            });
+        },
+        validators: { onSubmit: loginSchema },
+        validationLogic: revalidateLogic({
+            mode: "submit",
+            modeAfterSubmission: "change",
+        }),
+    });
+
+    const handleGoogleSignIn = () => {
+        authClient.signIn.social({
+            provider: "google",
+            callbackURL: `${window.location.origin}`,
+            fetchOptions: {
+                onSuccess: (ctx) => {
+                    toast.success(`Welcome, ${ctx.data.user.name}`);
+                },
+                onError: ({ error }) => {
+                    toast.error(error.message);
+                },
+            },
+        });
+    };
+
+    const lastMethod = authClient.getLastUsedLoginMethod();
+
+    return (
+        <Card className="w-full max-w-sm md:max-w-md">
+            <CardHeader className="text-center">
+                <CardTitle className="text-xl">Welcome back</CardTitle>
+                <CardDescription>
+                    Login with your Apple or Google account
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        form.handleSubmit;
+                    }}
+                >
+                    <FieldGroup>
+                        <Field>
+                            <Button
+                                className="relative"
+                                onClick={handleGoogleSignIn}
+                                type="button"
+                                variant="outline"
+                            >
+                                <div className="flex items-center gap-2">
+                                    <GoogleLogo />
+                                    <span>Login with Google</span>
+                                </div>
+
+                                {lastMethod === "google" && (
+                                    <Badge className="absolute -top-2 -right-1">
+                                        Last used
+                                    </Badge>
+                                )}
+                            </Button>
+                        </Field>
+
+                        <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                            Or continue with
+                        </FieldSeparator>
+
+                        <form.Field name="email">
+                            {(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+                                return (
+                                    <Field data-invalid={isInvalid}>
+                                        <FieldLabel htmlFor="email">
+                                            Email Address
+                                        </FieldLabel>
+
+                                        <Input
+                                            aria-invalid={isInvalid}
+                                            disabled={form.state.isSubmitting}
+                                            id={field.name}
+                                            name={field.name}
+                                            onBlur={field.handleBlur}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value
+                                                )
+                                            }
+                                            placeholder="Enter your email address"
+                                            type="email"
+                                            value={field.state.value}
+                                        />
+
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        </form.Field>
+
+                        <form.Field name="password">
+                            {(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+
+                                return (
+                                    <Field data-invalid={isInvalid}>
+                                        <div className="flex items-center">
+                                            <FieldLabel htmlFor="password">
+                                                Password
+                                            </FieldLabel>
+
+                                            <Link
+                                                className="ml-auto text-sm underline-offset-4 hover:underline"
+                                                to="/forgot-password"
+                                            >
+                                                Forgot your password?
+                                            </Link>
+                                        </div>
+
+                                        <Input
+                                            aria-invalid={isInvalid}
+                                            disabled={form.state.isSubmitting}
+                                            id={field.name}
+                                            name={field.name}
+                                            onBlur={field.handleBlur}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value
+                                                )
+                                            }
+                                            placeholder="Enter your password"
+                                            type="password"
+                                            value={field.state.value}
+                                        />
+
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        </form.Field>
+
+                        <form.Field name="rememberMe">
+                            {(field) => (
+                                <div className="flex items-center gap-2">
+                                    <Checkbox
+                                        checked={Boolean(field.state.value)}
+                                        id={field.name}
+                                        name={field.name}
+                                        onCheckedChange={(val) =>
+                                            field.handleChange(Boolean(val))
+                                        }
+                                    />
+
+                                    <FieldLabel htmlFor={field.name}>
+                                        Remember me
+                                    </FieldLabel>
+                                </div>
+                            )}
+                        </form.Field>
+
+                        <form.Subscribe>
+                            {(state) => (
+                                <Field>
+                                    <Button
+                                        aria-busy={isPending}
+                                        disabled={isPending || !state.canSubmit}
+                                        type="submit"
+                                    >
+                                        {isPending ? "Logging in..." : "Login"}
+                                    </Button>
+
+                                    <FieldDescription className="text-center">
+                                        Don&apos;t have an account?{" "}
+                                        <Link to="/register">Sign up</Link>
+                                    </FieldDescription>
+                                </Field>
+                            )}
+                        </form.Subscribe>
+                    </FieldGroup>
+                </form>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default LoginForm;
---
+++ src/components/features/auth/register-form.tsx
@@ -0,0 +1,241 @@
+import { revalidateLogic, useForm } from "@tanstack/react-form";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { useTransition } from "react";
+import toast from "react-hot-toast";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { registerSchema } from "@/schemas/auth-schema";
+import GoogleLogo from "./google-logo";
+
+const RegisterForm = () => {
+    const navigate = useNavigate();
+    const [isPending, startTransition] = useTransition();
+
+    const form = useForm({
+        defaultValues: { name: "", email: "", password: "" },
+        onSubmit: ({ value }) => {
+            startTransition(async () => {
+                await authClient.signUp.email(
+                    {
+                        name: value.name,
+                        email: value.email,
+                        password: value.password,
+                    },
+                    {
+                        onSuccess: (ctx) => {
+                            toast.success(`Welcome, ${ctx.data.user.name}`);
+                            navigate({ to: "/", replace: true });
+                        },
+                        onError: (ctx) => {
+                            toast.error(ctx.error.message);
+                        },
+                    }
+                );
+            });
+        },
+        validators: { onSubmit: registerSchema },
+        validationLogic: revalidateLogic({
+            mode: "change",
+            modeAfterSubmission: "change",
+        }),
+    });
+
+    const handleGoogleSignIn = () => {
+        authClient.signIn.social({
+            provider: "google",
+            callbackURL: `${window.location.origin}`,
+            fetchOptions: {
+                onSuccess: (ctx) => {
+                    toast.success(`Welcome, ${ctx.data.user.name}`);
+                },
+                onError: ({ error }) => {
+                    toast.error(error.message);
+                },
+            },
+        });
+    };
+
+    return (
+        <Card className="w-full max-w-sm md:max-w-md">
+            <CardHeader className="text-center">
+                <CardTitle className="text-xl">Create your account</CardTitle>
+                <CardDescription>
+                    Fill in the form below to create your account
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        form.handleSubmit();
+                    }}
+                >
+                    <FieldGroup>
+                        <form.Field name="name">
+                            {(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+
+                                return (
+                                    <Field data-invalid={isInvalid}>
+                                        <FieldLabel htmlFor={field.name}>
+                                            Full Name
+                                        </FieldLabel>
+                                        <Input
+                                            disabled={form.state.isSubmitting}
+                                            id={field.name}
+                                            name={field.name}
+                                            onBlur={field.handleBlur}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value
+                                                )
+                                            }
+                                            placeholder="Enter your full name"
+                                            type="text"
+                                            value={field.state.value}
+                                        />
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        </form.Field>
+
+                        <form.Field name="email">
+                            {(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+
+                                return (
+                                    <Field data-invalid={isInvalid}>
+                                        <FieldLabel htmlFor={field.name}>
+                                            Email Address
+                                        </FieldLabel>
+                                        <Input
+                                            disabled={form.state.isSubmitting}
+                                            id={field.name}
+                                            name={field.name}
+                                            onBlur={field.handleBlur}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value
+                                                )
+                                            }
+                                            placeholder="Enter your email"
+                                            type="email"
+                                            value={field.state.value}
+                                        />
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        </form.Field>
+
+                        <form.Field name="password">
+                            {(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+
+                                return (
+                                    <Field data-invalid={isInvalid}>
+                                        <FieldLabel htmlFor={field.name}>
+                                            Password
+                                        </FieldLabel>
+
+                                        <Input
+                                            disabled={form.state.isSubmitting}
+                                            id={field.name}
+                                            name={field.name}
+                                            onBlur={field.handleBlur}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value
+                                                )
+                                            }
+                                            placeholder="Enter your password"
+                                            type="password"
+                                            value={field.state.value}
+                                        />
+
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        </form.Field>
+
+                        <form.Subscribe>
+                            {(state) => (
+                                <Field>
+                                    <Button
+                                        aria-busy={isPending}
+                                        disabled={isPending || !state.canSubmit}
+                                        type="submit"
+                                    >
+                                        {isPending
+                                            ? "Signing up..."
+                                            : "Sign up"}
+                                    </Button>
+                                </Field>
+                            )}
+                        </form.Subscribe>
+
+                        <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                            Or continue with
+                        </FieldSeparator>
+
+                        <Field>
+                            <Button
+                                onClick={() => handleGoogleSignIn()}
+                                type="button"
+                                variant="outline"
+                            >
+                                <GoogleLogo />
+                                Sign up with Google
+                            </Button>
+                        </Field>
+
+                        <FieldDescription className="text-center">
+                            Already have an account?{" "}
+                            <Link to="/login">Sign in</Link>
+                        </FieldDescription>
+                    </FieldGroup>
+                </form>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default RegisterForm;
