5346e0dea3e0f348418efc0953f39ccee1833959
Arnold Agaba <arnoldagaba000@gmail.com>
2026-02-18 18:06:59 +0300

    feat: Add search validation schema for login route

--- src/routes/_auth/login.tsx
+++ src/routes/_auth/login.tsx
@@ -1,8 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import z from "zod";
 import LoginForm from "@/components/features/auth/login-form";
+
+const searchSchema = z.object({
+    redirectTo: z.string().optional(),
+});
 
 export const Route = createFileRoute("/_auth/login")({
     component: LoginPage,
+    validateSearch: searchSchema,
 });
 
 function LoginPage() {
