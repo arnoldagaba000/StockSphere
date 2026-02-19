49057a9e0f9e1bd6e5083b41e7dad838ab98567a
Arnold Agaba <arnoldagaba000@gmail.com>
2026-02-19 13:30:29 +0300

    fix: Add regex for password field.

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -1,8 +1,13 @@
 import z from "zod";
 
-const emailSchema = z.email();
-const passwordSchema = z.string();
-const newPasswordSchema = z.string().min(8);
+const emailSchema = z.email("Invalid email address");
+const passwordSchema = z
+    .string()
+    .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
+    );
+const newPasswordSchema = passwordSchema;
 
 export const registerSchema = z.object({
     name: z.string().min(4),
