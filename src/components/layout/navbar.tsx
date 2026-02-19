8db51eed9f04633edc588c263411cfe9df1891d7
Arnold Agaba <arnoldagaba000@gmail.com>
2026-02-19 10:49:58 +0300

    feat: Wrap SidebarTrigger and Separator in a flex container in Navbar component

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,9 +1,17 @@
+import { Separator } from "../ui/separator";
 import { SidebarTrigger } from "../ui/sidebar";
 
 const Navbar = () => {
     return (
         <header className="border-b px-4 py-3 shadow">
-            <SidebarTrigger />
+            <div className="flex items-center gap-3">
+                <SidebarTrigger />
+
+                <Separator
+                    className="mr-2 data-[orientation=vertical]:h-6"
+                    orientation="vertical"
+                />
+            </div>
         </header>
     );
 };
